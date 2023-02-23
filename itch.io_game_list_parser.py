2d1f529538142471310642efca6bb0f733c3eda7
gmh4589 <41452060+gmh4589@users.noreply.github.com>
2023-02-24 00:07:32 +0300

    Add files via upload

--- itch.io_game_list_parser.py
+++ itch.io_game_list_parser.py
@@ -1,22 +1,38 @@
-import requests
-import os.path
-import re
-from bs4 import BeautifulSoup as soup
-from selenium import webdriver as wb
+import time
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as soup
 
-link = 'https://itch.io/games/made-with-godot'
+link = 'https://itch.io/games/downloadable/made-with-godot'
 
-for k in range(10):
-	r = requests.get(link)
-	html = soup(r.content, 'html.parser')
+# Set up the webdriver and navigate to the page
+driver = webdriver.Chrome()
+driver.get(link)
 
-	list = html.select('.grid_outer')
+# 36 games on page
 
-for i in list:
-	game_name = i.select('.game_title')
-	
-for j in game_name:
-	print(j.text)
-		
+iter = 131
+
+# Scroll down the page a few times to load more content
+for i in range(iter):
+	driver.find_element_by_tag_name('body').send_keys(Keys.END)
+	print(str(i) + '/' + str(iter))
+	time.sleep(.5)
+
+# Extract the HTML content and parse it with BeautifulSoup
+html = driver.page_source
+html_soup = soup(html, 'html.parser')
+
+# Find the game titles
+game_titles = html_soup.select('.game_title')
+
+for title in game_titles:
+	with open('godot.txt', 'a') as result:
+		try:
+			#print(title.text)
+			result.write(title.text + '\n')
+		except UnicodeEncodeError: pass
+
+# Close the webdriver
+driver.quit()
 input('Press ENTER')
